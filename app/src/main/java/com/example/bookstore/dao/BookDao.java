83c1322dc884c8dff919b55c2315cb55226d5f2c
65wu <904795395@qq.com>
2021-06-11 16:24:57 +0800

    :card_file_box: card基本dao

---
+++ app/src/main/java/com/example/bookstore/dao/BookDao.java
@@ -0,0 +1,27 @@
+package com.example.bookstore.dao;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.bookstore.entity.Book;
+
+import java.util.List;
+
+@Dao
+public interface BookDao {
+    @Query("Select * from book")
+    List<Book> getAllBooks();
+    @Query("Select * from book where category_id = :category_id")
+    List<Book> getBooksByCategory(int category_id);
+    @Query("Select * from book where author = :author")
+    List<Book> getBooksByAuthor(String author);
+    @Insert
+    void insertBook(Book... book);
+    @Update
+    void updateBook(Book... book);
+    @Delete
+    void deleteBook(Book... book);
+}
