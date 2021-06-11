75e42fd0572dcfc9c1163511eb4825d6a45d24f8
65wu <904795395@qq.com>
2021-06-11 16:42:02 +0800

    :bug: 补充getter与setter

--- app/src/main/java/com/example/bookstore/entity/Book.java
+++ app/src/main/java/com/example/bookstore/entity/Book.java
@@ -3,6 +3,7 @@ package com.example.bookstore.entity;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.ForeignKey;
+import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
 @Entity(tableName = "book",
@@ -22,10 +23,59 @@ public class Book {
     @ColumnInfo(name = "category_id")
     private int category_id;
 
+    public Book(int id, String name, String description, String author, int category_id) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.author = author;
+        this.category_id = category_id;
+    }
+
+    @Ignore
     public Book(String name, String description, String author, int category_id) {
         this.name = name;
         this.description = description;
         this.author = author;
+        this.category_id = category_id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(int category_id) {
         this.category_id = category_id;
     }
 }
--- app/src/main/java/com/example/bookstore/entity/Category.java
+++ app/src/main/java/com/example/bookstore/entity/Category.java
@@ -2,6 +2,7 @@ package com.example.bookstore.entity;
 
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
 @Entity(tableName = "category")
@@ -11,8 +12,29 @@ public class Category {
     @ColumnInfo(name = "name")
     private String name;
 
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
+    @Ignore
     public Category(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 }
