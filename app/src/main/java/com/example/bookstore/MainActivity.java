1e765f4bb57e26b241d6c5a3fa81424aca80dfcb
65wu <904795395@qq.com>
2021-06-13 21:00:06 +0800

    :boom: 重整布局

--- app/src/main/java/com/example/bookstore/MainActivity.java
+++ app/src/main/java/com/example/bookstore/MainActivity.java
@@ -1,44 +1,48 @@
 package com.example.bookstore;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 import com.example.bookstore.db.BookStoreDataBase;
-import com.example.bookstore.entity.Book;
-import com.example.bookstore.entity.Category;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import org.jetbrains.annotations.NotNull;
 
-import com.example.bookstore.databinding.ActivityMainBinding;
+import cn.leancloud.AVUser;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        com.example.bookstore.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        setContentView(R.layout.activity_main);
 
         BookStoreDataBase appDb = BookStoreDataBase.getInstance(this);
-        Category c = new Category(1, "现代文学");
-        Book[] books = {new Book("书名", "描述", "作者", 1)};
-        appDb.categoryDao().insertCategory(c);
-        appDb.bookDao().insertBook(books);
-        appDb.bookDao().getAllBooks();
+//        appDb.clearAllTables();
+//        Category c = new Category(1, "现代文学");
+//        Book[] books = {new Book("书名", "描述", "作者", 1)};
+//        appDb.categoryDao().insertCategory(c);
+//        appDb.bookDao().insertBook(books);
+//        appDb.bookDao().getAllBooks();
+
+
+        AVUser user = new AVUser();
+        user.setUsername("test_user");
+        user.setEmail("test@qq.com");
+        user.setPassword("123456");
+
+        user.signUpInBackground().subscribe(new Observer<AVUser>() {
+            public void onSubscribe(@NotNull Disposable disposable) {}
+            public void onNext(@NotNull AVUser user) {
+                // 注册成功
+                System.out.println("注册成功。objectId：" + user.getObjectId());
+            }
+            public void onError(@NotNull Throwable throwable) {
+                System.out.println(throwable);
+            }
+            public void onComplete() {}
+        });
     }
 }
