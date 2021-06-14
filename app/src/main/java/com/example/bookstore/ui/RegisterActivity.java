376d8ee3891c6b6feb317b21f33e499bb89d5760
65wu <904795395@qq.com>
2021-06-14 15:15:14 +0800

    :sparkles: 初步完成注册通信

--- app/src/main/java/com/example/bookstore/ui/RegisterActivity.java
+++ app/src/main/java/com/example/bookstore/ui/RegisterActivity.java
@@ -1,8 +1,11 @@
 package com.example.bookstore.ui;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,12 +13,14 @@ import com.example.bookstore.R;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
+
 import cn.leancloud.AVUser;
+import es.dmoral.toasty.Toasty;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
 
 public class RegisterActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,11 +37,23 @@ public class RegisterActivity extends AppCompatActivity {
 
             public void onNext(@NotNull AVUser user) {
                 // 注册成功
-                System.out.println("注册成功。objectId：" + user.getObjectId());
+                Toasty.success(getApplicationContext(),
+                        "注册成功，请验证邮箱。",
+                        Toast.LENGTH_SHORT,
+                        true).show();
             }
 
             public void onError(@NotNull Throwable throwable) {
-                System.out.println(throwable);
+                String message = throwable.getMessage();
+                assert message != null;
+                if(message.equals("Username has already been taken.")) {
+                    message = "此用户名已被占用。";
+                }
+                Toasty.error(
+                        getApplicationContext(),
+                        message,
+                        Toast.LENGTH_SHORT,
+                        true).show();
             }
 
             public void onComplete() {
