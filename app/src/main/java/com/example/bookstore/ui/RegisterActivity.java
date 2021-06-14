2611b2f5a5750b27d5a03ddc0e6772b2858a352a
65wu <904795395@qq.com>
2021-06-14 16:11:11 +0800

    :sparkles: 完成注册

--- app/src/main/java/com/example/bookstore/ui/RegisterActivity.java
+++ app/src/main/java/com/example/bookstore/ui/RegisterActivity.java
@@ -1,19 +1,16 @@
 package com.example.bookstore.ui;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.bookstore.R;
+import com.example.bookstore.databinding.LayoutRegisterBinding;
 
 import org.jetbrains.annotations.NotNull;
-
-import java.util.Objects;
 
 import cn.leancloud.AVUser;
 import es.dmoral.toasty.Toasty;
@@ -21,43 +18,55 @@ import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
 
 public class RegisterActivity extends AppCompatActivity {
+    private com.example.bookstore.databinding.LayoutRegisterBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        AVUser user = new AVUser();
-        user.setUsername("test_user");
-        user.setEmail("test@qq.com");
-        user.setPassword("123456");
+        binding = LayoutRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        user.signUpInBackground().subscribe(new Observer<AVUser>() {
-            public void onSubscribe(@NotNull Disposable disposable) {
-            }
+        binding.registerButton.setOnClickListener(view -> {
+            String username = binding.username.getText().toString();
+            String mobile_number = binding.mobileNumber.getText().toString();
+            String email = binding.email.getText().toString();
+            String password = binding.password.getText().toString();
 
-            public void onNext(@NotNull AVUser user) {
-                // 注册成功
-                Toasty.success(getApplicationContext(),
-                        "注册成功，请验证邮箱。",
-                        Toast.LENGTH_SHORT,
-                        true).show();
-            }
+            AVUser user = new AVUser();
+            user.setUsername(username);
+            user.setMobilePhoneNumber(mobile_number);
+            user.setEmail(email);
+            user.setPassword(password);
 
-            public void onError(@NotNull Throwable throwable) {
-                String message = throwable.getMessage();
-                assert message != null;
-                if(message.equals("Username has already been taken.")) {
-                    message = "此用户名已被占用。";
+            user.signUpInBackground().subscribe(new Observer<AVUser>() {
+                public void onSubscribe(@NotNull Disposable disposable) {
                 }
-                Toasty.error(
-                        getApplicationContext(),
-                        message,
-                        Toast.LENGTH_SHORT,
-                        true).show();
-            }
 
-            public void onComplete() {
-            }
+                public void onNext(@NotNull AVUser user) {
+                    // 注册成功
+                    Toasty.success(getApplicationContext(),
+                            "注册成功，请验证邮箱。",
+                            Toast.LENGTH_SHORT,
+                            true).show();
+                }
+
+                public void onError(@NotNull Throwable throwable) {
+                    String message = throwable.getMessage();
+                    assert message != null;
+                    if(message.equals("Username has already been taken.")) {
+                        message = "此用户名已被占用。";
+                    }
+                    Toasty.error(
+                            getApplicationContext(),
+                            message,
+                            Toast.LENGTH_SHORT,
+                            true).show();
+                }
+
+                public void onComplete() {
+                }
+            });
         });
     }
 
