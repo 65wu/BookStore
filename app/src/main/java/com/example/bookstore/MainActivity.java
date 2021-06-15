f057c754ab53d891c8e4cf3ec76091c1a36ba9f3
65wu <904795395@qq.com>
2021-06-15 14:07:12 +0800

    :sparkles: 完成navigation_view切换

--- app/src/main/java/com/example/bookstore/MainActivity.java
+++ app/src/main/java/com/example/bookstore/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.bookstore;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.bookstore.db.BookStoreDataBase;
 import com.example.bookstore.ui.LoginActivity;
 import com.example.bookstore.ui.NoInternetActivity;
+import com.example.bookstore.util.BottomNavigationBehavior;
 import com.example.bookstore.util.NetworkUtil;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+import org.jetbrains.annotations.NotNull;
 
 import cn.leancloud.AVUser;
 
@@ -21,6 +33,26 @@ public class MainActivity extends AppCompatActivity {
             AVUser currentUser = AVUser.getCurrentUser();
             if (currentUser != null) {
                 setContentView(R.layout.activity_main);
+                Toolbar toolbar = findViewById(R.id.toolbar);
+                setSupportActionBar(toolbar);
+
+
+                DrawerLayout drawer = findViewById(R.id.drawer_layout);
+                ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                        this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+                drawer.addDrawerListener(toggle);
+                toggle.syncState();
+
+                NavigationView navigationView = findViewById(R.id.nav_view);
+                navigationView.setNavigationItemSelectedListener(navigationItemSelectedListener);
+
+                BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+                bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavigationItemSelectedListener);
+//
+                CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) bottomNavigationView.getLayoutParams();
+                layoutParams.setBehavior(new BottomNavigationBehavior());
+
+                bottomNavigationView.setSelectedItemId(R.id.navigationHome);
             } else {
                 // 跳到登录页面
                 Intent intent = new Intent(this, LoginActivity.class);
@@ -38,4 +70,27 @@ public class MainActivity extends AppCompatActivity {
 //        appDb.bookDao().insertBook(books);
 //        appDb.bookDao().getAllBooks();
     }
+    private final BottomNavigationView.OnNavigationItemSelectedListener bottomNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+            if (item.getItemId() == R.id.navigationMenu) {
+                    DrawerLayout drawer = findViewById(R.id.drawer_layout);
+                    drawer.openDrawer(GravityCompat.START);
+                    return true;
+                }
+            return false;
+        }
+    };
+
+    private final NavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new NavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+            if (item.getItemId() == R.id.navigationMenu) {
+                DrawerLayout drawer = findViewById(R.id.drawer_layout);
+                drawer.openDrawer(GravityCompat.START);
+                return true;
+            }
+            return false;
+        }
+    };
 }
---
+++ app/src/main/java/com/example/bookstore/util/BottomNavigationBehavior.java
@@ -0,0 +1,63 @@
+package com.example.bookstore.util;
+
+import android.content.Context;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import org.jetbrains.annotations.NotNull;
+
+public class BottomNavigationBehavior extends CoordinatorLayout.Behavior<BottomNavigationView> {
+
+    public BottomNavigationBehavior() {
+        super();
+    }
+
+    public BottomNavigationBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean layoutDependsOn(@NotNull CoordinatorLayout parent, @NotNull BottomNavigationView child, @NotNull View dependency) {
+        return dependency instanceof FrameLayout;
+    }
+
+    @Override
+    public boolean onStartNestedScroll(
+            @NotNull CoordinatorLayout coordinatorLayout,
+            @NotNull BottomNavigationView child,
+            @NotNull View directTargetChild,
+            @NotNull View target,
+            int nestedScrollAxes,
+            int i) {
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL;
+    }
+
+    @Override
+    public void onNestedPreScroll(
+            @NotNull CoordinatorLayout coordinatorLayout,
+            @NotNull BottomNavigationView child,
+            @NotNull View target,
+            int dx,
+            int dy,
+            @NotNull int[] consumed,
+            int i
+    ) {
+        if (dy < 0) {
+            showBottomNavigationView(child);
+        } else if (dy > 0) {
+            hideBottomNavigationView(child);
+        }
+    }
+
+    private void hideBottomNavigationView(BottomNavigationView view) {
+        view.animate().translationY(view.getHeight());
+    }
+
+    private void showBottomNavigationView(BottomNavigationView view) {
+        view.animate().translationY(0);
+    }
+}
